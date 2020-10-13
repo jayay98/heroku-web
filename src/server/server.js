509cfc715d2ddd3fe778719ada755746d2679261
Jay <jayay98@gmail.com>
2020-10-13 23:48:23 +0800

    Add helmetjs support

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const helmet = require('helmet');
 
 const app = express();
 const port = process.env.PORT || "1337";
+app.use(helmet());
 app.set("port", port);
 
 app.use("/", express.static('public'));
